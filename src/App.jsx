36d5991f799af6d7b0aeb07e5b1369c2e1dcfd17
salargholami <salargholami021@gmail.com>
2024-02-04 04:51:41 +0330

    Adding First Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
-import './App.css'
+import { Provider } from "react-redux";
+import "./App.css";
+import store from "./redux/store";
+import CakeContainer from "./components/cakeContainer";
 
 function App() {
-
-
   return (
-    <div>
-      
-    </div>
-  )
+    <Provider store={store}>
+      <div>Redux demo</div>
+      <CakeContainer />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,10 @@
+function CakeContainer() {
+  return (
+    <div>
+      <h2>Cake Number : 10</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+export default CakeContainer;
